69e99ebb0a13955f35ef66a4ce7665730231ff59
MinhThien <nguyenanminhthien97@gmail.com>
2025-02-11 10:30:06 +0700

    Finish latest-file and re-create repeate at file main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from itertools import repeat
 # usage python main.py -con yes -pages 300   -proxy yes  // This is used for scrape with proxy servers list.
 # usage python main.py  -pages 30 -start 0 -end 2 -proxy no  //This is used for scrape with a range of pages only.
 
-def latest_file():
+def latest_file(number_of_pages):
     if os.name == "nt":
         # window
         filepath = os.getcwd() + "\\output\\" + "*.csv"
@@ -20,11 +20,17 @@ def latest_file():
         # other
         filepath = os.getcwd() + "/output/" + "*.csv"
     list_of_files = glob.glob(filepath)  # * means all if need specific format then *.csv
-    latest_file = max(list_of_files, key=os.path.getctime)
     if os.name == "nt":
-        return latest_file.split("\\")[-1]
+        file_numbers = [int(file.split("\\")[-1].split("-")[1]) // number_of_pages for file in list_of_files]
+        file_numbers.sort()
     else:
-        return latest_file.split("/")[-1]
+        file_numbers = [int(file.split('/')[-1].split("-")[1]) // number_of_pages for file in list_of_files]
+        file_numbers.sort()
+    for i in range(os.cpu_count()):
+        count_down =file_numbers.__len__() -1 - i
+        if file_numbers[count_down] == count_down:
+            interrup_file = f"output-{count_down * number_of_pages}-{(count_down + 1) * number_of_pages}.csv"
+            return interrup_file
 
 
 def welcome():
@@ -53,7 +59,7 @@ def devide_part(start, stop):
 def create_repeats(start, end):
     repeats = list()
     # each CPU will run the devide_part 2 times.
-    for j in range(0, ((end + 1 - start) // (2 * os.cpu_count()))):
+    for j in range(start // (2*os.cpu_count()), ((end + 1 - start) // (2 * os.cpu_count()))):
         repeats.append([i for i in range(j * (2 * os.cpu_count()), (j + 1) * (2 * os.cpu_count()))])
 
     final = (end + 1 - start) % (2 * os.cpu_count())
@@ -91,7 +97,7 @@ if __name__ == '__main__':
     # Define the start and end in a range if we decide to run with proxy and scrape all data.
     if args.start == None and args.end == None:
         if con.lower() == "yes":
-            latest_file = latest_file()
+            latest_file = latest_file(number_pages)
             start = round(int(latest_file.split("-")[2].strip(".csv")) / number_pages)
             end = round(9889953 / number_pages) - 1
         #     From practical calculation, end - 1 for stop before running in forever
